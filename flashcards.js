5eb66063f130aeb482cb3df7732f195b621cb6a4
mkacik <m.m.kacik@gmail.com>
2025-01-08 17:16:38 -0600

    V0: tap to flip the card, then tap to select new card

---
+++ flashcards.js
@@ -0,0 +1,73 @@
+const DECK = `
+A,あ
+I,い
+U,う
+E,え
+O,お
+
+KA,か
+KI,き
+KU,く
+KE,け
+KO,こ
+`;
+
+// transform deck string from CSV format to object storing indexed pairs of side A and B
+function parseDeck(deckString) {
+  let deck = new Map();
+  let rows = deckString.trim().split("\n");
+
+  var index = 0;
+  for (row of rows) {
+    sides = row.split(",");
+    if (sides.length == 2) {
+      deck.set(index, [sides[0], sides[1]]);
+      // only increment index on valid rows
+      index += 1;
+    }
+  }
+
+  return deck;
+}
+
+function getContainer() {
+  return document.getElementById("root");
+}
+
+function setUp() {
+  let deck =  parseDeck(DECK);
+  console.log(deck);
+  window.FLASHCARDS = {}
+  FLASHCARDS.deck = deck;
+
+  let container = getContainer();
+  container.innerHTML = '<img src="play.svg" alt="START" width="240" height="240">';
+  container.onclick = newCard;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getRandomCard() {
+  let randomIndex = getRandomInt(FLASHCARDS.deck.size);
+  return FLASHCARDS.deck.get(randomIndex);
+}
+
+function newCard() {
+  let container = getContainer();
+  let card = getRandomCard();
+
+  // pick side to show first
+  let [sideA, sideB] = (getRandomInt(2) == 0) ? [card[0], card[1]] : [card[1], card[0]];
+  container.innerHTML = sideA;
+  container.onclick = function() { flipCard(sideA, sideB); };
+}
+
+function flipCard(sideA, sideB) {
+  let container = getContainer();
+  container.innerHTML = sideA + "<br \>" + sideB;
+  container.onclick = newCard;
+}
+
+document.addEventListener("DOMContentLoaded", setUp);
