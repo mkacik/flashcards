79e9efac073dddd9acf7337b46a3c34854861f3e
mkacik <m.m.kacik@gmail.com>
2025-01-23 14:47:41 -0600

    Move static -> docs because that's the only dirname choice I get with github pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/App.tsx',
   output: {
-    path: path.resolve(__dirname, 'static'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js'
   },
   module: {
