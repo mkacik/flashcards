8bcd7dda3adb6764353d2fa81525548ed6f86e1b
mkacik <m.m.kacik@gmail.com>
2025-02-04 14:02:44 -0600

    Katakana can be now practiced

--- src/PracticeSession.tsx
+++ src/PracticeSession.tsx
@@ -1,60 +1,72 @@
 import React from "react";
 import { useState } from "react";
-import { FrontSide, PersistentSettings } from "./Settings";
+import { FrontSide, Kana, PersistentSettings } from "./Settings";
 import { Deck, Card } from "./Deck";
 
 const getRandomInt = (max: number): number => {
   return Math.floor(Math.random() * max);
 };
 
-type PickFrontSideFunction = (card: Card) => string;
-
-function getPickFrontSideFunction(
-  settings: PersistentSettings,
-): PickFrontSideFunction {
-  switch (settings.frontSide) {
-    case FrontSide.ENGLISH:
-      return (card: Card) => card.english;
-    case FrontSide.KANA:
-      return (card: Card) => card.hiragana;
-    case FrontSide.RANDOM:
-      return (card: Card) => (getRandomInt(2) == 0 ? card.english : card.hiragana);
-  }
+const coinToss = (): boolean => {
+  return (getRandomInt(2) == 0);
 }
 
-function pickCard(deck: Deck): Card {
+function getFrontSide(settings: PersistentSettings): FrontSide {
+  const frontSide = settings.frontSide;
+
+  if (frontSide === FrontSide.RANDOM) {
+    return coinToss() ? FrontSide.ENGLISH : FrontSide.KANA;
+  }
+  return frontSide;
+}
+
+function wrap(...args: string[]) {
+  return <>
+    {args.map((item, index) => <div key={index}>{item}</div>)}
+  </>;
+}
+
+// Deck card has 3 sides - en, hiragana & katakana. In practice session context, card should only
+// have two sides, so I use following struct and function to pick new deck card and turn it to
+// 2-sided flashcard.
+type SessionCard = {
+  front: React.ReactElement;
+  back: React.ReactElement;
+};
+
+function pickCard(deck: Deck, settings: PersistentSettings): SessionCard {
   const cardsArray = Array.from(deck.values());
   const randomIndex = getRandomInt(cardsArray.length);
-  const card = cardsArray[randomIndex];
-  return card;
+  const deckCard = cardsArray[randomIndex];
+
+  const english = deckCard.english;
+  const kana = (settings.kana == Kana.HIRAGANA) ? deckCard.hiragana : deckCard.katakana;
+
+  if (getFrontSide(settings) === FrontSide.KANA) {
+    return {
+      front: wrap(kana),
+      back: wrap(english),
+    } as SessionCard;
+  }
+
+  // TODO: if front is english, look up for possible same spelling characters and put them
+  // both on card back side
+  return {
+    front: wrap(english),
+    back: wrap(kana),
+  } as SessionCard;
 }
 
 export function PracticeSession(
   { deck, settings, bumpCardCount }:
   { deck: Deck, settings: PersistentSettings, bumpCardCount: () => void },
 ): React.ReactNode {
-  const pickFrontSide = getPickFrontSideFunction(settings);
-
-  const [card, setCard] = useState<Card>(pickCard(deck));
-  const [text, setText] = useState<React.ReactNode>(pickFrontSide(card));
+  const [card, setCard] = useState<SessionCard>(pickCard(deck, settings));
   const [flipped, setFlipped] = useState<boolean>(false);
 
-  const flipCard = () => {
-    const text = (
-      <>
-        <div>{card.hiragana}</div>
-        <div>{card.english}</div>
-      </>
-    );
-    setText(text);
-    setFlipped(true);
-  };
-
   const pickNewCard = () => {
-    const card = pickCard(deck);
-    const text = <div>{pickFrontSide(card)}</div>;
+    const card = pickCard(deck, settings);
     setCard(card);
-    setText(text);
     setFlipped(false);
     bumpCardCount();
   };
@@ -62,9 +74,9 @@ export function PracticeSession(
   return (
     <div
       className="card-content"
-      onClick={() => (flipped ? pickNewCard() : flipCard())}
+      onClick={() => (flipped ? pickNewCard() : setFlipped(true))}
     >
-      {text}
+      {flipped ? card.back : card.front}
     </div>
   );
 }
